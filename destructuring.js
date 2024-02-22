e177593de89c1fbbcb51aa414da19801a0172c33
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-22 12:07:21 +0600

    learning about destructuring

---
+++ destructuring.js
@@ -0,0 +1,11 @@
+const actor = {
+  name: "ananta",
+  age: 30,
+  phone: "018238283",
+  money: 12344566,
+};
+
+const phone = actor.phone;
+const name = actor.name;
+const age = actor.age;
+console.log(phone, name, age);
