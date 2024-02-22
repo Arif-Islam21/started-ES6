7900c4719b58c8520837464fcb93cc82f43e4562
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-22 16:03:01 +0600

    opened in a new file

--- practise4.js
+++
@@ -1,8 +0,0 @@
-const result = (arr1, arr2) => {
-  const newArr = [...arr1, ...arr2];
-  const max = Math.max(...newArr);
-  console.log(newArr);
-  console.log(max);
-};
-
-result([1, 2, 34, 43], [32, 43, 45, 12]);
