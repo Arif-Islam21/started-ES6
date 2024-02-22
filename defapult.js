a74f5737ece71c57b66985e3d326bea226ab989c
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-22 08:41:57 +0600

    learning default

---
+++ defapult.js
@@ -0,0 +1,10 @@
+// default => if value is not provided , take as a default
+
+function add(num1, num2) {
+  const result = num1 + num2;
+  console.log(num1, num2, result);
+  return result;
+}
+
+// const sum = add(5, 6, 7);
+const sum = add(5, 6);
