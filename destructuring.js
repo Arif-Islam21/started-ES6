3be7e5db89523f473c5d44d625e38b1110a008b2
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-22 12:24:05 +0600

    learned about basic destructuring

--- destructuring.js
+++ destructuring.js
@@ -5,7 +5,28 @@ const actor = {
   money: 12344566,
 };
 
-const phone = actor.phone;
-const name = actor.name;
-const age = actor.age;
-console.log(phone, name, age);
+// UNDERSTAING DESTRUCTURING
+// if right sight is an object then left side also will be an object
+const { phone, age: boyosh, name } = actor;
+
+// const phone = actor.phone;
+// const name = actor.name;
+// const age = actor.age;
+console.log(phone);
+console.log(boyosh);
+console.log(name);
+
+const numbers = [45, 87, 45];
+const [first, secoend] = numbers;
+const [x, y] = [12, 66];
+
+console.log(first);
+console.log(secoend);
+
+function doubleThem(a, b) {
+  return [a * 2, b * 2];
+}
+
+const [prothom, ditiyo] = doubleThem(4, 6);
+console.log(prothom);
+console.log(ditiyo);
