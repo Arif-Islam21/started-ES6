016c071e0987850fe25b93c9617682b4010ebd54
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-22 08:49:10 +0600

    learned about default paramiter in function

--- defapult.js
+++ defapult.js
@@ -1,10 +1,23 @@
 // default => if value is not provided , take as a default
 
-function add(num1, num2) {
+function add(num1 = 99, num2 = 0) {
   const result = num1 + num2;
   console.log(num1, num2, result);
   return result;
 }
 
 // const sum = add(5, 6, 7);
-const sum = add(5, 6);
+const sum = add(3);
+
+function fullName(first = " ", last = " ") {
+  const full = first + " " + last;
+  return full;
+}
+
+function friends(numbs = []) {
+  console.log(numbs);
+}
+
+function person(arr = {}) {
+  console.log(arr);
+}
