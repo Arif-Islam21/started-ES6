871c12509befa153d8840fe3c2d330228869d732
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-22 09:26:43 +0600

    skip for now...4 done

--- arrow.js
+++ arrow.js
@@ -18,6 +18,10 @@ const add2 = function (a, b) {
 // ARROW FUNCTION
 
 const add3 = (a, b) => a + b;
+const add4 = (num1, num2, num3, num4) => num1 + num2 + num3 + num4;
+
+const sum2 = add4(1, 2, 3, 4);
+console.log(sum2);
 
 const sum = add3(5, 90);
 console.log(sum);
