e957eb25d6651fa5daaeb7c1be62ef92c024eb4b
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-23 08:47:49 +0600

    learned about class

---
+++ module-31/class.js
@@ -0,0 +1,21 @@
+const products = [
+  { id: 1, name: "lenovo", price: 65000 },
+  { id: 2, name: "hp", price: 55000 },
+  { id: 3, name: "dell", price: 25000 },
+  { id: 4, name: "Apple", price: 615000 },
+];
+
+class product {
+  constructor(name) {
+    this.name = this.name;
+  }
+
+  country = "Bangladesh";
+  speak(talk) {
+    console.log(`talking about ${talk}`);
+  }
+}
+
+const lenovo = new product("lele lenovo");
+lenovo.speak("bakum bakum");
+console.log(lenovo);
