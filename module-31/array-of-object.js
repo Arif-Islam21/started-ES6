f33089fb0c55b8b7a8f03668ed63f2b036be7920
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-23 08:14:48 +0600

    learned about some method

---
+++ module-31/array-of-object.js
@@ -0,0 +1,6 @@
+const products = [
+  { id: 1, name: "lenovo", price: 65000 },
+  { id: 2, name: "hp", price: 55000 },
+  { id: 3, name: "dell", price: 25000 },
+  { id: 4, name: "Apple", price: 615000 },
+];
