c175dd516bacf2a2940f974a884ddd22956b3fd1
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-23 15:09:56 +0600

    done today

--- module-31/this.js
+++ module-31/this.js
@@ -1,17 +1,26 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  sleep() {
-    console.log(`this is ${this.name} sleeping now`);
-  }
-  activity() {
-    this.sleep();
-  }
-}
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   sleep() {
+//     console.log(`this is ${this.name} sleeping now`);
+//   }
+//   activity() {
+//     this.sleep();
+//   }
+// }
 
-const kodom = new Person("Kodom  ali", 21);
-console.log(kodom);
-kodom.sleep();
-kodom.activity();
+// const kodom = new Person("Kodom  ali", 21);
+// console.log(kodom);
+// kodom.sleep();
+// kodom.activity();
+
+// function min(nums) {
+//   return Math.min(nums);
+// }
+// console.log(min([1, 3, 2]));
+
+const nums = [1, 2, 3, 4, 5];
+let output = nums.filter((n) => n % 2);
+console.log(output);
