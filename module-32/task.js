1ea84de7aeb383aa42945705dee6214eb4da3595
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-26 09:54:32 +0600

    task 1 done with map

--- module-32/task.js
+++ module-32/task.js
@@ -1,7 +1,25 @@
 const numbers = [1, 3, 5, 7, 9];
-for (let num of numbers) {
-  if (num % 2 === 1) {
-    num += 1;
-  }
-  console.log(num);
-}
+// for (let num of numbers) {
+//   if (num % 2 === 1) {
+//     num += 1;
+//   }
+//   console.log(num);
+// }
+
+// numbers.map(() => {
+//   if (num % 2 === 1) {
+//     num = num + 1;
+//   }
+//   console.log(num);
+// });
+
+const even = numbers.map(
+  (num = (n) => {
+    if (n % 2 === 1) {
+      n += 1;
+    }
+    return n;
+  })
+);
+
+console.log(even);
