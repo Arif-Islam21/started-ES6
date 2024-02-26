0eb16be487f369f66d442e57dedf0721727979f1
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-26 09:59:37 +0600

    task 2 done with array.filter method

--- module-32/task.js
+++ module-32/task.js
@@ -1,4 +1,4 @@
-const numbers = [1, 3, 5, 7, 9];
+// const numbers = [1, 3, 5, 7, 9];
 // for (let num of numbers) {
 //   if (num % 2 === 1) {
 //     num += 1;
@@ -13,13 +13,24 @@ const numbers = [1, 3, 5, 7, 9];
 //   console.log(num);
 // });
 
-const even = numbers.map(
-  (num = (n) => {
-    if (n % 2 === 1) {
-      n += 1;
+// const even = numbers.map(
+//   (num = (n) => {
+//     if (n % 2 === 1) {
+//       n += 1;
+//     }
+//     return n;
+//   })
+// );
+
+// console.log(even);
+
+const numbers = [33, 50, 79, 78, 90, 101, 30];
+
+const result = numbers.filter(
+  (divide = (n) => {
+    if (n % 10 === 0) {
+      return n;
     }
-    return n;
   })
 );
-
-console.log(even);
+console.log(result);
