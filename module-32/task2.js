d5ad154dc8ed373abaa10f58d1437457f6322009
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-26 10:14:40 +0600

    learned basic reduce method

--- module-32/task2.js
+++ module-32/task2.js
@@ -1,20 +1,33 @@
-const instructor = [
-  { name: "Nodi", age: 28, position: "senior" },
-  { name: "Akil", age: 26, position: "junior" },
-  { name: "Shobuj", age: 30, position: "senior" },
+// const instructor = [
+//   { name: "Nodi", age: 28, position: "senior" },
+//   { name: "Akil", age: 26, position: "junior" },
+//   { name: "Shobuj", age: 30, position: "senior" },
+// ];
+
+// let seniorName = [];
+
+// for (let num in instructor) {
+//   const instructorValue = instructor[num];
+//   const positionValue = instructorValue.position;
+//   if (positionValue === "senior") {
+//     seniorName.push(instructorValue.name);
+//   }
+// }
+// console.log(seniorName);
+
+const people = [
+  { name: "Meena", age: 32 },
+  { name: "Reena", age: 42 },
+  { name: "Shokina", age: 23 },
 ];
 
-let seniorName = [];
+const nums = [20, 15, 22];
+const sum = nums.reduce(
+  (total = (prev, current) => {
+    const sums = prev + current;
+    return sums;
+  }),
+  0
+);
 
-for (let num in instructor) {
-  const instructorValue = instructor[num];
-  const positionValue = instructorValue.position;
-  if (positionValue === "senior") {
-    seniorName.push(instructorValue.name);
-    // console.log("senior", positionValue);
-  }
-  //   console.log(positionValue);
-}
-// const positionValue = instructor[0];
-// console.log(positionValue);
-console.log(seniorName);
+console.log(sum);
